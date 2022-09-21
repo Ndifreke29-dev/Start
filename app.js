c769f2d4db779b5e4393ae94705395a328b49458
Ndifreke <ndifrekesamuel15@gmail.com>
2022-09-21 14:05:08 +0100

    Commit

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// Initialize variable - OPEN BUTTON
 const openBtn = document.querySelector('#toggle');
+  //  Add Event Listener
   openBtn.addEventListener('mouseover', firstFunction);
 
+    // Create Function
     function firstFunction() {
       document.querySelector('.sidebar').style.width = '150px';
     }
 
+    // CLOSE BUTTON
 const closeBtn = document.querySelector('#close');
   closeBtn.addEventListener('click', secondFunction);
 
@@ -12,8 +16,10 @@ const closeBtn = document.querySelector('#close');
       document.querySelector('#sidebar-toggle').style.width = '0px';
     }
 
+
+// BODY CLOSE
 document.addEventListener("click", myFunction);
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+  document.getElementById('sidebar-toggle').style.width = '0px';
 }
